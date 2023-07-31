daf92e6f52c36e631b4cf2f1c556fe7a36e98d46
masudfcs1 <57311382+masudfcs1@users.noreply.github.com>
2023-07-31 20:05:08 +0600

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import authRoutes from "./routes/authRoute.js";
 import cors from "cors";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
+import path from 'path';
 
 dotenv.config();
 
@@ -17,14 +18,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to ecommerce app</h1>");
-});
+app.use('*', function(req,res){
+  res.sendFile(path.join(__dirnamem,'./client/build/index.html'))
+})
 
 const PORT = process.env.PORT || 8080;
 
